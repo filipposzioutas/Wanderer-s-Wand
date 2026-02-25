6aacc9ca2c81331d6e7ba87037bacce11154fc31
filipposzioutas <filippos.zioutas@gmail.com>
2026-02-25 08:59:49 +0200

    SwitchandRestart.cs

---
+++ SwitchandRestart.cs
@@ -0,0 +1,19 @@
+SwitchandRestart.cs
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchandRestart : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
